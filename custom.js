4fc919d8b353045a5ffcb349245112c527e6b8b9
MdHasan0467 <ornilhasan.oht.riyad@gmail.com>
2022-07-15 00:30:00 +0600

    first commit

---
+++ custom.js
@@ -0,0 +1,10 @@
+  var navLinks = document.getElementById("navLinks");
+
+
+  function showMenu() {
+  navLinks.style.right = "0"; //beacause of we want to change the right value//
+  }
+
+  function hideMenu() {
+  navLinks.style.right = "-200px"; //beacause of we want to change the right value//
+  }
